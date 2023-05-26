c22e8148cc2aa6d6c0935af997d6aea68ded488c
Patrick-Ngige <wakemanjajr@gmail.com>
2023-05-26 11:25:11 +0300

    added:login restriction & hide wp-login

---
+++ wp-content/themes/Assessment7/404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+
+<div style="width:100w; margin-top:10px; margin-left:15rem; display:flex; justify-content:center; align-items:center; height:90vh;  background-image: url('https://images.unsplash.com/photo-1578328819058-b69f3a3b0f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8NDA0JTIwZXJyb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60');background-size:contain; background-repeat:no-repeat; ">
+</div>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/Assessment7/functions.php
@@ -0,0 +1,82 @@
+<?php
+
+//LIMITTING LOGIN ATTEMPTS
+function attempted_login($user, $username, $password)
+{
+    if (get_transient('attempted_login')) {
+        $datas = get_transient('attempted_login');
+
+        if ($datas['tried'] >= 5) {
+
+            $until = get_option('_transient_timeout_' . 'attempted_login');
+
+            $time = time_to_go($until);
+
+            return new WP_Error('too_many_tried', sprintf(__('<strong>ERROR</strong>: Your trial limit has been reached, please try after %1$s'), $time));
+        }
+    }
+    return $user;
+}
+
+add_filter('authenticate', 'attempted_login', 30, 3);
+
+function login_failed($username)
+{
+    if (get_transient('attempted_login')) {
+        $datas = get_transient('attempted_login');
+
+        $datas['tried']++;
+
+        if ($datas['tried'] <= 3) {
+            set_transient('attempted_login', $datas, 300);
+        } else {
+            $datas = array(
+                'tried' => 1,
+            );
+            set_transient('attempted_login', $datas, 300);
+            
+        }
+    }
+}
+
+add_action('wp_login_failed', 'login_failed', 10, 1);
+
+function time_to_go($timestamp)
+{
+    $periods = [
+        "second",
+        "minute",
+        "hour",
+        "day",
+        "month",
+        "year"
+    ];
+
+    $lengths = [
+        "60",
+        "60",
+        "24",
+        "7",
+        "4.35",
+        "12",
+    ];
+
+    $current_timestamp = time();
+    $difference = abs($current_timestamp - $timestamp);
+
+    for ($i = 0; $difference >= $lengths[$i] && $i < count($lengths) - 1; $i++) {
+        $difference /= $lengths[$i];
+    }
+
+    //COUNT DOWN
+
+    $difference = round($difference);
+
+    if (isset($difference)) {
+        if ($difference != 1) {
+            $periods[$i] .= "s";
+            $output = "$difference $periods[$i]";
+            return $output;
+        }
+    }
+}
